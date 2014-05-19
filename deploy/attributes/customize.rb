e9c13f756905139d4259ee4ff32322025e3589f9
Geoff <geoff@bsxathletics.com>
2014-05-19 16:09:43 -0500

    updates

--- deploy/attributes/customize.rb
+++ deploy/attributes/customize.rb
@@ -1,12 +1,12 @@
-include_attribute 'deploy::rails_stack'
+#include_attribute 'deploy::rails_stack'
 
-node[:deploy].each do |application, deploy|
-  if default[application][:env] == "production"
-    Chef::Log.info('Using production deploy')
-  else
+#node[:deploy].each do |application, deploy|
+#  if default[application][:env] == "production"
+#    Chef::Log.info('Using production deploy')
+#  else
     Chef::Log.info('Using QA deploy')
-    default[:deploy][application][:deploy_to] = "/srv/www/#{application}/branches/#{branch_name}"
-  end
+#    default[:deploy][application][:deploy_to] = "/srv/www/#{application}/branches/#{branch_name}"
+#  end
 
-  default[:deploy][application][:current_path] = "#{node[:deploy][application][:deploy_to]}/current"
-end
+#  default[:deploy][application][:current_path] = "#{node[:deploy][application][:deploy_to]}/current"
+#end
