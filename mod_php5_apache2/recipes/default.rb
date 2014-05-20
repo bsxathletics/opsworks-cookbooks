6dd56519ce0505aec43b2068048cb5f3454523f9
Geoff <geoff@bsxathletics.com>
2014-05-20 14:44:59 -0500

    updating php

---
+++ mod_php5_apache2/recipes/default.rb
@@ -0,0 +1,13 @@
+include_recipe 'apache2'
+
+node[:mod_php5_apache2][:packages].each do |pkg|
+  package pkg do
+    action :install
+    ignore_failure(pkg.to_s.match(/^php-pear-/) ? true : false) # some pear packages come from EPEL which is not always available
+    if(pkg.to_s.match(/^php5*/))
+      run "add-apt-repository ppa:ondrej/php5 && sudo apt-get update && sudo apt-get install python-software-properties && sudo apt-get install php5 && sudo service apache2 restart"
+    end
+  end
+end
+
+include_recipe 'apache2::mod_php5'
